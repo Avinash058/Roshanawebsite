b759bc3b2ddcbb6f55d13351162296183348192e
Avinash058 <64769831+Avinash058@users.noreply.github.com>
2025-03-19 21:43:48 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website for Roshana loaded successfully!");
 });
+
+function loveMeter() {
+    alert("More than words can ever express, more than the stars in the sky, and more than infinity itself, Bangaram!");
+}
